eb4f435e902db9f5c52acda9fa8b80589a2f9a23
Ólafur Örn Nielsen <olafur@form5.is>
2013-08-30 12:56:35 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    connect: {
+      server: {
+        options: {
+          port: 1337,
+          base: 'dist',
+          keepalive: true
+        }
+      }
+    },
+
+    rubyHaml: {
+      dist: {
+        files: grunt.file.expandMapping(['views/*.haml'], 'dist/', {
+          rename: function(base, path) {
+            return base + path.replace(/\.haml$/, '.html').replace('views/', '');
+          }
+        })
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'dist/assets/stylesheets/main.css' : 'assets/stylesheets/main.scss'
+        }
+      }
+    },
+
+    watch: {
+      haml: {
+        files: ['views/**/*.haml'],
+        tasks: ['rubyHaml'],
+        options: {
+          livereload: true
+        }
+      },
+      sass: {
+        files: ['assets/stylesheets/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.task.loadNpmTasks('grunt-ruby-haml');
+  grunt.task.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['rubyHaml', 'sass'])
+
+}
+
+
+
